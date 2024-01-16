a0451d9d5b850ae14dfda1a09b3fc5254f98176a
Flavia-20 <flaviamonique38@gmail.com>
2024-01-15 23:14:58 -0300

    Adição de comentários
    sobre os metodos catch e forEach

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const containerVideos = document.querySelector(".videos__container");
+const containerVideos = document.querySelector(".videos__container");//Isso servirá para capturar a ul que acabamos de criar em nosso código HTML.
 
 const api = fetch("http://localhost:3000/videos")
-.then(res => res.json())
-.then((videos) =>
+.then(res => res.json())//Então, quando recebermos a resposta da nossa busca transformada em JSON...
+.then((videos) => // ... executaremos o que está dentro deste .then: uma arrow function chamando videos.
     videos.forEach((video)=> {
+        /*Nosso objetivo é buscar o container que pegamos do HTML. Então, copiaremos containerVideos, 
+        colaremos no .forEach e adicionaremos .innerHTML. Com isso, estamos pedindo que o .innerHTML 
+        coloque os vídeos dentro do HTML.
+        Em seguida, vamos inserir +=, para que cada vídeo seja acrescentado no innerHTML */
         containerVideos.innerHTML += `
         <li class="videos__item">
             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
@@ -16,6 +20,6 @@ const api = fetch("http://localhost:3000/videos")
         `;
     })
 )
-.catch((error) => {
+.catch((error) => { //essa função é usada para capturar os erros que acontecerem 
     containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error} </p>`;
 })
